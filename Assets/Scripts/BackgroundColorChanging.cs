c23100ab45cf6a41be956e411dbcd73a7438c26f
Nitropical <paulbarbier295@gmail.com>
2026-02-01 14:47:07 +0100

    Color changing

--- Assets/Scripts/BackgroundColorChanging.cs
+++ Assets/Scripts/BackgroundColorChanging.cs
@@ -11,8 +11,14 @@ public class BackgroundColorChanging : MonoBehaviour
     [Header("Hue Control")]
     [Range(0f, 1f)]
     public float hue = 0f;
+    [SerializeField] private float duration;
 
     private ColorAdjustments colorAdjustments;
+
+    private void Start()
+    {
+        hue = 0f;
+    }
 
     void OnEnable()
     {
@@ -30,6 +36,9 @@ public class BackgroundColorChanging : MonoBehaviour
     {
         if (colorAdjustments == null)
             return;
+
+        hue += Time.deltaTime / duration;
+        hue = Mathf.Repeat(hue, 0.99f);
 
         Color color = Color.HSVToRGB(hue, 0.35f, 1f);
         colorAdjustments.colorFilter.value = color;
